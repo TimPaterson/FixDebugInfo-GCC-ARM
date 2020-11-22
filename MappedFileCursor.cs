634b4d86364158988888c8ad30aa9df29ee79bd2
Tim <tp@timpaterson.com>
2020-11-22 09:56:11 -0800

    Add project files.

---
+++ MappedFileCursor.cs
@@ -0,0 +1,154 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace FixDebugInfo
+{
+	class MappedFileCursor
+	{
+		#region Constructors
+
+		public MappedFileCursor(MemoryMappedViewAccessor view, uint offset, uint size)
+		{
+			Next = offset;
+			m_view = view;
+			m_end = offset + size;
+		}
+
+		public MappedFileCursor(MappedFileCursor cursor, uint offset, uint size)
+		{
+			Next = offset;
+			m_view = cursor.m_view;
+			m_end = offset + size;
+		}
+
+		public MappedFileCursor(MappedFileCursor cursor, uint size)
+		{
+			Next = cursor.Next;
+			m_view = cursor.m_view;
+			m_end = Next + size;
+		}
+
+		public MappedFileCursor(MappedFileCursor cursor)
+		{
+			Next = cursor.Next;
+			m_view = cursor.m_view;
+			m_end = cursor.m_end;
+		}
+
+		public MappedFileCursor(MemoryMappedViewAccessor view)
+		{
+			m_view = view;
+		}
+
+		#endregion
+
+
+		#region Public Fields
+
+		public uint Next;
+
+		#endregion
+
+
+		#region Properties
+
+		public bool AtEnd { get { return Next >= m_end; } }
+		public MemoryMappedViewAccessor View { get { return m_view; } }
+		public uint End { get { return m_end; } }
+
+		#endregion
+
+
+		#region Private Fields
+
+		uint m_end;
+		MemoryMappedViewAccessor m_view;
+
+		#endregion
+
+
+		#region Public Methods
+
+		public void Read<T>(out T res) where T : struct
+		{
+			m_view.Read(Next, out res);
+			Next += (uint)Marshal.SizeOf<T>();
+		}
+
+		public byte ReadByte()
+		{
+			byte b = m_view.ReadByte(Next);
+			Next += sizeof(byte);
+			return b;
+		}
+
+		public ushort ReadUshort()
+		{
+			ushort us = m_view.ReadUInt16(Next);
+			Next += sizeof(ushort);
+			return us;
+		}
+
+		public uint ReadUint()
+		{
+			uint u = m_view.ReadUInt32(Next);
+			Next += sizeof(uint);
+			return u;
+		}
+
+		public string ReadString()
+		{
+			StringBuilder stb;
+			char ch;
+
+			stb = new StringBuilder();
+			for (; ; )
+			{
+				ch = (char)m_view.ReadByte(Next++);
+				if (ch == 0)
+					break;
+				stb.Append(ch);
+			}
+			return stb.ToString();
+		}
+
+		public string ReadString(uint off)
+		{
+			Next = off;
+			return ReadString();
+		}
+
+		public void Write<T>(ref T res) where T : struct
+		{
+			m_view.Write(Next, ref res);
+			Next += (uint)Marshal.SizeOf<T>();
+		}
+
+		public void WriteByte(byte b)
+		{
+			m_view.Write(Next, b);
+			Next += sizeof(byte);
+		}
+
+		public void WriteUshort(ushort us)
+		{
+			m_view.Write(Next, us);
+			Next += sizeof(ushort);
+		}
+
+		public void WriteUint(uint u)
+		{
+			m_view.Write(Next, u);
+			Next += sizeof(uint);
+		}
+
+		public void WriteByteArray(byte[] arb)
+		{
+			m_view.WriteArray(Next, arb, 0, arb.Length);
+			Next += (uint)arb.Length;
+		}
+
+		#endregion
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FixDebugInfo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ElfReader elf;
+			DwarfReader dwarf;
+			DwarfReader.LineResult lineRes;
+			MappedFileCursor sectionCursor;
+			MappedFileCursor unitCursor;
+			ElfReader.SectionInfo section;
+			uint pos;
+			uint size;
+			bool fDumpOnly = false;
+
+			Debug.WriteLine(args[0]);
+			if (args.Length > 1 && args[1] == "/d")
+				fDumpOnly = true;
+			elf = new ElfReader(args[0]);
+			dwarf = new DwarfReader(elf);
+			section = dwarf.OpenDebugLines();
+			sectionCursor = section.Cursor;
+
+			while (!sectionCursor.AtEnd)
+			{
+				size = dwarf.GetLineInfoSize(sectionCursor);
+				if (size == 0)
+					continue;	// skip unit, cursor already advanced
+
+				unitCursor = new MappedFileCursor(sectionCursor, size);
+				pos = unitCursor.Next;
+				sectionCursor.Next += size;
+				lineRes = dwarf.GetLineInfo(unitCursor);
+				if (fDumpOnly)
+				{
+					DumpLines(lineRes);
+					continue;
+				}
+
+				lineRes.lines = dwarf.FilterLines(lineRes.lines);
+				//DumpLines(lineRes);
+				unitCursor.Next = pos;
+				dwarf.GenLineInfo(unitCursor, lineRes.lines);
+			}
+			elf.Close();
+		}
+
+		static void DumpLines(DwarfReader.LineResult lines)
+		{
+			int i;
+
+			Console.WriteLine("Files:");
+			i = 1;
+			foreach (string file in lines.files)
+			{
+				Console.WriteLine($"{i} {file}");
+				i++;
+			}
+
+			Console.WriteLine("Line List:");
+			foreach (DwarfReader.LineItem line in lines.lines)
+			{
+				Console.Write($"addr: {line.Address:X}");
+				Console.Write($", line: {line.Line}");
+				Console.Write($", file: {line.File}");
+				Console.Write($", stmt: {line.IsStmt}");
+				Console.Write($", col: {line.Column}");
+				Console.Write($", discr: {line.Discr}");
+				if (line.IsEnd)
+					Console.Write(", end");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
+	}
+}
